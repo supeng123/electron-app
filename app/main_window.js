a8da80a666778b6846d990bbeb1e1c03ccce014a
supeng123 <492526873@qq.com>
2019-07-15 09:16:44 +0800

    finish the timer-tray app

---
+++ app/main_window.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+const { BrowserWindow } = electron;
+
+class MainWindow extends BrowserWindow {
+
+    constructor(url) {
+        super({
+            webPreferences: {
+                nodeIntegration: true,
+                backgroundThrottling: false
+            },
+            height: 500,
+            width: 300,
+            frame: false,
+            resizable: false,
+            show: false
+        });
+        this.loadURL(url);
+
+        this.on('blur', this.onBlur.bind(this));
+    }
+    
+    onBlur() {
+        this.hide();
+    }
+
+}
+
+module.exports = MainWindow;
--- index.js
+++ index.js
@@ -1,42 +1,26 @@
 const electron = require('electron');
 const path = require('path');
 
-const { app, BrowserWindow, Tray} = electron;
+const { app, ipcMain} = electron;
+
+const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: true
-        },
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    if (process.platform === 'darwin') app.dock.hide();
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
-    tray = new Tray(iconPath);
 
-    tray.on('click', (event, bounds) => {
-        const {x,y} = bounds;
-        const {height, width} = mainWindow.getBounds();
+    tray = new TimerTray(iconPath, mainWindow);
 
-        if (mainWindow.isVisible()) {
-            mainWindow.hide();
-        } else {
-            // mainWindow.setBounds({
-            //     x: x - width/2,
-            //     y: process.platform === 'darwin' ? y : y - height,
-            //     height,
-            //     width
-            // });
-            mainWindow.show();
-        }
-    });
+});
+
+
+ipcMain.on('update-timer', (event, timeLeft) => {
+    tray.setTitle(timeLeft);
 });
